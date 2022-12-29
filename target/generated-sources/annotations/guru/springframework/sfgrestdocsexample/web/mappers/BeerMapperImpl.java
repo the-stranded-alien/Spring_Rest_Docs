d6ba8cb0d85f7763e2fca35088f8ca0f4b93a821
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-29 20:33:22 +0530

    Documentation Generation

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-29T20:18:03+0530",
+    date = "2022-12-29T20:32:35+0530",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.4.1 (Oracle Corporation)"
 )
 @Component
