883c8e7f833bc580fcbea73c97d1db23233c31a0
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-29 18:53:21 +0530

    Documenting Path Params

---
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgrestdocsexample.web.mappers;
+
+import guru.springframework.sfgrestdocsexample.domain.Beer;
+import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-12-29T18:43:15+0530",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.4.1 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto BeerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        return beerDto;
+    }
+
+    @Override
+    public Beer BeerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Beer beer = new Beer();
+
+        return beer;
+    }
+}
